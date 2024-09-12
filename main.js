a7e60325cf9c57331a30d8fb931c71f9700f624a
Kamal <tangudukamal1729@gmail.com>
2024-09-12 20:21:15 +0530

    result css

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('weatherForm');
-    const resultDiv = document.getElementById('weatherResult');
-    const apiKey = 'YOUR_API_KEY'
-    // const apiKey = '304fb3d5a4286003b841c2146b7dc997'; 
+    const resultdiv = document.getElementById('weatherResult');
+    const tempElement = document.getElementById('temp');
+    const weatherDescElement = document.getElementById('weatherDesc');
+    const humidityElement = document.getElementById('humidity');
+    const windSpeedElement = document.getElementById('windSpeed');
+    const cityElement = document.getElementById('city-header');
+    const apiKey = 'Your_API_KEY'
+    //const apiKey = '304fb3d5a4286003b841c2146b7dc997'; 
     const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
     form.addEventListener('submit', async (event) => {
@@ -16,16 +21,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 throw new Error('City not found or API request failed');
             }
             const data = await response.json();
+            resultdiv.style.display = 'grid';
+            cityElement.innerHTML = `Weather in ${data.name}`;
+            tempElement.innerHTML = `<i class="fas fa-temperature-high"></i> Temperature: ${data.main.temp} °C`;
+            weatherDescElement.innerHTML = `<i class="fas fa-cloud-sun"></i> Weather: ${data.weather[0].description}`;
+            humidityElement.innerHTML = `<i class="fas fa-tint"></i> Humidity: ${data.main.humidity}%`;
+            windSpeedElement.innerHTML = `<i class="fas fa-wind"></i> Wind Speed: ${data.wind.speed} m/s`;
 
-            resultDiv.innerHTML = `
-                <h2>Weather in ${data.name}</h2>
-                <p>Temperature: ${data.main.temp} °C</p>
-                <p>Weather: ${data.weather[0].description}</p>
-                <p>Humidity: ${data.main.humidity}%</p>
-                <p>Wind Speed: ${data.wind.speed} m/s</p>
-            `;
         } catch (error) {
-            resultDiv.innerHTML = `<p>Error: ${error.message}</p>`;
+            // Handle errors, e.g., display an error message
+            tempElement.innerHTML = `<p>Error: ${error.message}</p>`;
+            weatherDescElement.innerHTML = '';
+            humidityElement.innerHTML = '';
+            windSpeedElement.innerHTML = '';
         }
     });
 });
